29d74844bef29f00c46bd078743b247293a421c4
kkd <kkd@kkds-MacBook-Pro.local>
2021-12-14 00:17:46 -0600

    Finish up dashboard

--- src/app/views/pages/dashboard/dashboard.module.ts
+++ src/app/views/pages/dashboard/dashboard.module.ts
@@ -6,12 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { FeahterIconModule } from 'src/app/core/feather-icon/feather-icon.module';
 import { NgbDropdownModule, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
-// Ng-ApexCharts
-import { NgApexchartsModule } from "ng-apexcharts";
-
-// Ng2-charts
-import { ChartsModule } from 'ng2-charts';
-
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
@@ -29,9 +23,7 @@ const routes: Routes = [
     FormsModule,
     FeahterIconModule,
     NgbDropdownModule,
-    NgbDatepickerModule,
-    NgApexchartsModule,
-    ChartsModule
+    NgbDatepickerModule
   ]
 })
 export class DashboardModule { }
